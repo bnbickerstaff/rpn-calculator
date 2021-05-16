6dfd264f9c9783fe283f71d2ad791edcd4216be4
bbick <84204053+branbick@users.noreply.github.com>
2021-05-16 18:27:03 -0400

    Cleaned up and added additional mathematical operations

--- calculator.py
+++ calculator.py
@@ -1,24 +1,25 @@
-# Base class the *RPN* calculator class derives from
+# Import statements
 from enum import Enum
+import math
 
+# Enum used by `input_type` method
 class InputType(Enum):
     NUMBER = 1
     STRING = 2
     INVALID = 3
 
+# Base class the *RPN* calculator class derives from
 class Calculator:
-    # Mathematical operations
+    # Instance methods
+    ## Mathematical (in alphabetical order)
     def add(self, addend1, addend2):
         return addend1 + addend2 # sum
 
-    def subtract(self, minuend, subtrahend):
-        return minuend - subtrahend # difference
+    def divide(self, dividend, divisor):
+        return dividend / divisor # quotient
 
     def multiply(self, factor1, factor2):
         return factor1 * factor2 # product
-
-    def divide(self, dividend, divisor):
-        return dividend / divisor # quotient
 
     def power(self, base, exponent):
         return base ** exponent # power
@@ -26,12 +27,21 @@ class Calculator:
     def reciprocal(self, num):
         return 1 / num # reciprocal
 
-    # TODO: Starting simple. Add more operations (e.g., exp and square) later.
+    def square(self, num):
+        return num ** 2 # square
 
-    # Other instance methods
+    def square_root(self, num):
+        return math.sqrt(num) # square root
+
+    def subtract(self, minuend, subtrahend):
+        return minuend - subtrahend # difference
+
+    # TODO: Start simple and add more later
+
+    ## Other
     def input_type(self, input):
         input_type = None
-        if type(input) == int or type(input) == float: # Not including `complex` for now
+        if type(input) == int or type(input) == float: # Not including `complex` type (for now)
             input_type = InputType.NUMBER
         elif type(input) == str:
             input_type = InputType.STRING
