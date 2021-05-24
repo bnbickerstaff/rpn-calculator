244117b8f38ba51eab99c96e0c5093de5d29e63f
bbick <84204053+branbick@users.noreply.github.com>
2021-05-24 16:54:36 -0400

    Seems to work!

--- rpn_calculator.py
+++ rpn_calculator.py
@@ -1,10 +1,17 @@
 # Import statements
+from enum import Enum
 from math import isnan
+
+class InputType(Enum):
+    NUMBER = 1
+    STRING = 2
 
 class RPNCalculator():
     # Class variables
-    VALID_OPERATIONS = {'+', '-', '*', '/', 'drop', 'roll', 'swap'} # TODO: Start simple and add more later
-    INVALID_INPUT_LIMIT = 3 # After this many invalid inputs, calculator will "turn off"
+    VALID_ONE_ARG_OPERATIONS = {'drop'} # TODO: Start simple and add more later
+    VALID_TWO_ARG_OPERATIONS = {'+', '-', '*', '/', 'roll', 'swap'} # TODO: Start simple and add more later
+    VALID_OPERATIONS = VALID_ONE_ARG_OPERATIONS.union(VALID_TWO_ARG_OPERATIONS)
+    INVALID_INPUT_LIMIT = 3 # After this many invalid inputs, calculator will "shut down"
 
     # Constructor
     def __init__(self):
@@ -16,23 +23,74 @@ class RPNCalculator():
     def run(self):
         while True:
             raw_input = self.get_input()
-            processed_input = self.process_input(raw_input)
-            pass # TODO
+            refined_input = self.refine_input(raw_input)
+            input_is_valid = self.validate_input(refined_input)
+            if input_is_valid == False:
+                if self.invalid_input_cnt == RPNCalculator.INVALID_INPUT_LIMIT:
+                    print("Invalid input limit exceeded. Shutting down.")
+                    break
+                else:
+                    continue
+            else:
+                self.process_input(refined_input)
+            self.print_stack()
 
     def get_input(self):
         print("Enter operation/number: ", end = "")
-        raw_input = input().strip()
+        raw_input = input().strip().lower()
+
         return raw_input
 
-    def process_input(self, raw_input):
-        processed_input = None
+    def refine_input(self, raw_input):
+        refined_input = {}
         try:
-            processed_input = float(raw_input)
-            if processed_input.is_integer():
-                processed_input = int(processed_input)
+            refined_input['value'] = float(raw_input)
+            if refined_input['value'].is_integer():
+                refined_input['value'] = int(refined_input['value'])
+            refined_input['type'] = InputType.NUMBER
         except:
-            processed_input = raw_input
-        return processed_input # Either a float, int, or str
+            refined_input['value'] = raw_input
+            refined_input['type'] = InputType.STRING
+
+        return refined_input
+
+    def validate_input(self, refined_input):
+        self.invalid_input_cnt += 1
+        input_is_valid = False
+        if len(self.stack) == 0 and refined_input['type'] != InputType.NUMBER:
+            print("ERROR: Stack is empty. Cannot perform entered operation.")
+        elif refined_input['type'] == InputType.STRING and len(self.stack) != 0 \
+            and refined_input['value'] not in RPNCalculator.VALID_OPERATIONS:
+            print("ERROR: Entered operation not supported.")
+        elif refined_input['type'] == InputType.STRING and len(self.stack) == 1 \
+            and refined_input['value'] not in RPNCalculator.VALID_ONE_ARG_OPERATIONS:
+            print("ERROR: Cannot perform entered operation with only one element in stack.")
+        else:
+            self.invalid_input_cnt -= 1
+            input_is_valid = True
+
+        return input_is_valid
+
+    def process_input(self, refined_input):
+        if refined_input['type'] == InputType.NUMBER:
+            self.stack.append(refined_input['value'])
+        else: # refined_input['type'] == InputType.STRING
+            operation = refined_input['value']
+            len_stack = len(self.stack)
+            if operation == 'drop':
+                self.stack.pop()
+            elif operation == '+':
+                self.stack.append(self.stack.pop() + self.stack.pop())
+            elif operation == '-':
+                self.stack.append(-(self.stack.pop() - self.stack.pop()))
+            elif operation == '*':
+                self.stack.append(self.stack.pop() * self.stack.pop())
+            elif operation == '/':
+                self.stack.append(1 / (self.stack.pop() / self.stack.pop()))
+            elif operation == 'roll':
+                self.stack.insert(0, self.stack.pop())
+            elif operation == 'swap':
+                self.stack[len_stack - 2], self.stack[len_stack - 1] = self.stack[len_stack - 1], self.stack[len_stack - 2]
 
     def print_stack(self):
         print("Stack:")
@@ -40,6 +98,5 @@ class RPNCalculator():
             print(num)
 
     # TODOS
-    # - Make `validate_input` function with optional arg for required input type (e.g., number for very first input)
-    # - Certain operations require a stack length of at least 2 (e.g., add). Check for that.
+    # - Create class var for stack length limit and check against it
     
